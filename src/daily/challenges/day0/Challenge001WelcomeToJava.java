9434fc98ca2bd7aa2eec996d656ed4569e18a28f
sruiNascimento <sruy19@gmail.com>
2022-10-13 23:07:53 -0300

    Desafios hackerrank com java

---
+++ src/daily/challenges/day0/Challenge001WelcomeToJava.java
@@ -0,0 +1,8 @@
+package daily.challenges.day0;
+
+public class Challenge001WelcomeToJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
